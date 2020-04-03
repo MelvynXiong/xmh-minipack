062459435303379c1736c78858839ed7e211aeeb
xiongmeihui <xiongmeihui@youzan.com>
2020-04-03 15:37:09 +0800

    feat: 增加测试文件集

---
+++ entry/index.js
@@ -0,0 +1,4 @@
+import greet from "./source/greet/index.js";
+import { intro } from "./source/intro/index.js";
+
+console.log(greet, intro);
---
+++ entry/source/greet/index.js
@@ -0,0 +1,4 @@
+import { obj } from './obj.js';
+import { begin } from './begin.js'
+
+export default greet = `${begin} ${obj}`;
